38bed667bb255d2866795c2cfa48e8557f22b91a
Angel Guzman <4n70n1017xd@gmail.com>
2025-05-30 12:36:01 -0600

    Agregando console log de depuracion

--- src/encolamiento/encolamiento.service.ts
+++ src/encolamiento/encolamiento.service.ts
@@ -109,6 +109,7 @@ export class EncolamientoService implements OnModuleInit {
         let result = 1;
         for (let i = 2; i <= n; i++) {
             result *= i;
+            console.log(result);
         }
         return result;
     }
